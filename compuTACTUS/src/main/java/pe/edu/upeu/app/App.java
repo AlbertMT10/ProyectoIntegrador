976e516e867ac7d41eef1328f6b28bf15046cf93
AlbertMT10 <frankcoaquirajusto4@gmail.com>
2022-07-19 02:26:39 -0500

    modificado

--- compuTACTUS/src/main/java/pe/edu/upeu/app/App.java
+++ compuTACTUS/src/main/java/pe/edu/upeu/app/App.java
@@ -1,13 +1,14 @@
 package pe.edu.upeu.app;
 
+import pe.edu.upeu.gui.MainGUI;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        new MenuOpciones().login();
+        // new MainGUI();
     }
 }
